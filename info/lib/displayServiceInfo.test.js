8469f52b961ed29b7486176508a54821e87dae6e
Jukka Gustafsson <jukka.gustafsson@nordcloud.com>
2019-01-13 21:54:24 +0200

    test(displayServiceInfo.test.js): added test for v1beta2 function type.

--- info/lib/displayServiceInfo.test.js
+++ info/lib/displayServiceInfo.test.js
@@ -104,7 +104,7 @@ describe('DisplayServiceInfo', () => {
         resources: [
           { type: 'resource.which.should.be.filterered', name: 'someResource' },
           { type: 'gcp-types/cloudfunctions-v1:projects.locations.functions', name: 'my-service-dev-func1' },
-          { type: 'gcp-types/cloudfunctions-v1:projects.locations.functions', name: 'my-service-dev-func2' },
+          { type: 'cloudfunctions.v1beta2.function', name: 'my-service-dev-func2' },
         ],
       };
 
